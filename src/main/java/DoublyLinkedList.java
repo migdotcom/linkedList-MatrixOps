5bb88759af6a332c508fa316e7e78cbb14c6f077
Miguel Dominguez <migueld225@gmail.com>
2020-09-19 17:10:53 -0500

    Homework 2

--- src/main/java/DoublyLinkedList.java
+++ src/main/java/DoublyLinkedList.java
@@ -1,27 +1,48 @@
 import java.io.*;
 
 public class DoublyLinkedList<E> {
-    private static class Node<E> {
+
+     class Node<E> {
         private E element;
         private Node<E> prev;
         private Node<E> next;
 
         public Node(E e, Node<E> p, Node<E> n) {
-            element = e;prev = p;next = n; }
+            element = e;
+            prev = p;
+            next = n;
+        }
 
-        public E getElement() { return element; }
+        public E getElement() {
+            return element;
+        }
 
-        public Node<E> getPrev() { return prev; }
+        public Node<E> getPrev() {
+            return prev;
+        }
 
-        public Node<E> getNext() { return next; }
+        public Node<E> getNext() {
+            return next;
+        }
 
-        public void setPrev(Node<E> p) { prev = p; }
+        public void setPrev(Node<E> p) {
+            prev = p;
+        }
 
-        public void setNext(Node<E> n) { next = n; }
+        public void setNext(Node<E> n) {
+            next = n;
+        }
     }
 
-    public boolean isEmpty() { return size == 0; }
-    public int size() { return size; }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+
+    public int size() {
+        return size;
+    }
 
     private Node<E> header;
     private Node<E> tail;
@@ -33,6 +54,8 @@ public class DoublyLinkedList<E> {
         header.setNext(tail);
     }
 
+
+
     public E first() {
         if (isEmpty()) return null;
         return header.getNext().getElement();
@@ -42,8 +65,14 @@ public class DoublyLinkedList<E> {
         if (isEmpty()) return null;
         return tail.getPrev().getElement();
     }
-    public void addFirst(E e) { addBetween(e, header, header.getNext()); }
-    public void addLast(E e) { addBetween(e, tail.getPrev(), tail); }
+
+    public void addFirst(E e) {
+        addBetween(e, header, header.getNext());
+    }
+
+    public void addLast(E e) {
+        addBetween(e, tail.getPrev(), tail);
+    }
 
     public static void answer(String output, String content) throws IOException {
         FileWriter writer = new FileWriter(output);
@@ -52,47 +81,34 @@ public class DoublyLinkedList<E> {
         bufferedWriter.close();
 
     }
+
+
     public static void read(String input, DoublyLinkedList listToRead) throws IOException {
         FileReader reader = new FileReader(input);
         BufferedReader bufferedReader = new BufferedReader(reader);
         while ((input = bufferedReader.readLine()) != null) {
-            String[] splitInt = input.split(" ");
-            for (String tmpWord : splitInt) {
-                listToRead.addFirst(Integer.parseInt(tmpWord));
+            String[] row = input.split(" ");
+            for (String tmpWord : row) {
+                listToRead.addLast(Integer.parseInt(tmpWord));
                 System.out.println(tmpWord);
             }
         }
     }
 
-    public E removeFirst( ) {
-        if (isEmpty()) return null;
-     return remove(header.getNext());
-     }
 
-     public E removeLast( ) {
-        if (isEmpty()) return null;
-     return remove(tail.getPrev());
-    }
-
-    private E remove(Node<E> node) {
-        Node<E> predecessor = node.getPrev( );
-        Node<E> successor = node.getNext( );
-        predecessor.setNext(successor);
-        successor.setPrev(predecessor);
-        size--;
-        return node.getElement( );
-    }
     private void addBetween(E e, Node<E> predecessor, Node<E> successor) {
         Node<E> newest = new Node<>(e, predecessor, successor);
-         predecessor.setNext(newest);
-         successor.setPrev(newest);
-         size++; }
+        predecessor.setNext(newest);
+        successor.setPrev(newest);
+        size++;
+    }
+
+
     public static void main(String[] args) throws IOException {
         DoublyLinkedList list =new DoublyLinkedList();
         String input="/Users/migdotcom/IdeaProjects/HW2/src/test/resources/sample_input_output/1_a.txt";
         list.read(input, list);
-
-
+        System.out.println(list.size());;
 
     }
 }
--- src/test/java/linkedListTest.java
+++ src/test/java/linkedListTest.java
@@ -9,37 +9,42 @@ import static org.junit.Assert.*;
 public class linkedListTest {
 //given when then
 
-//    private linkedList list;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        list = new linkedList();
-//    }
-//    private void givenAListWithContaining(int... elements) {
-//        for (int i :elements) {
-//            list.add(i);
-//        }
-//    }
-//    @Test
-//    public void nothing() throws Exception {
-//        assertEquals(0, list.size());
-//
-//    }
-//
-//    @Test
-//    public void testGivenOne_elementSizeIsOne() throws Exception {
-//        givenAListWithContaining(1);
-//        assertEquals(1,list.size());
-//
-//
-//    }
-//    @Test
-//    public void testGivenTwo_elementSizeIsTwo() throws Exception {
-//        givenAListWithContaining(1,2);
-//        assertEquals(2,list.size());
-//    }
-//
-//    @Test
+    private DoublyLinkedList list;
+
+
+    @Before
+    public void setUp() throws Exception {
+        list = new DoublyLinkedList();
+    }
+    private void givenAListWithContaining(int... elements) {
+        for (int i :elements) {
+            list.addFirst(i);
+        }
+    }
+    @Test
+    public void nothing() throws Exception {
+        assertEquals(0, list.size());
+
+    }
+
+    @Test
+    public void testGivenOne_elementSizeIsOne() throws Exception {
+        givenAListWithContaining(1);
+        assertEquals(1,list.size());
+
+
+    }
+    @Test
+    public void testGivenTwo_elementSizeIsTwo() throws Exception {
+        givenAListWithContaining(1,2);
+        assertEquals(2,list.size());
+    }
+
+    @Test
+    public void create_row_based_matrix() throws Exception {
+
+    }
+    //    @Test
 //    public void getOne_Value() throws Exception {
 //        givenAListWithContaining(1);
 //        int result = list.get(0);
